397651ea0e48f3eb5bb1106911a7b3f9084c3c16
Rikard-784 <victoriadurham19@outlook.com>
2024-11-02 12:30:44 +0000

    Revise testing framework: Update tests for better coverage.

--- game.go
+++ game.go
@@ -1,39 +1,96 @@
 package main
 
+
+
 import (
+
 	"math/rand"
+
 )
 
+
+
 var wordCategories = map[string][]string{
+
 	"animals":     {"dog", "cat", "elephant", "giraffe"},
- // Let’s hope for the best.	"countries":   {"canada", "brazil", "france", "japan"},
+
+	"countries":   {"canada", "brazil", "france", "japan"},
+
 	"programming": {"golang", "python", "javascript", "java"},
+
 }
+
+
 
 func selectRandomWord(category string) string {
+
 	words, exists := wordCategories[category]
+
 	if !exists {
+
 		return ""
+
 	}
+
 	return words[rand.Intn(len(words))]
+
 }
+
+
 
 func displayWord(word string, guessedLetters []rune) {
+
 	for _, letter := range word {
+
 		if contains(guessedLetters, letter) {
+
 			print(string(letter), " ")
+
 		} else {
+
 			print("_ ")
+
 		}
+
 	}
+
 	println()
+
 }
 
+
+
 func displayGuessedLetters(guessedLetters []rune) {
+
 	print("Guessed letters: ")
+
 	for _, letter := range guessedLetters {
+
 		print(string(letter), " ")
+
 	}
+
 	println()
+
 }
+
+
+
+func isWordGuessed(word string, guessedLetters []rune) bool {
+
+	for _, letter := range word {
+
+		if !contains(guessedLetters, letter) {
+
+			return false
+
+		}
+
+	}
+
+	return true
+
+}
+
+
 
