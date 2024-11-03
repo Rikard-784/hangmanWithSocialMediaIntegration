d6036ce3473446ab210238f355f14d5095e09714
Rikard-784 <victoriadurham19@outlook.com>
2024-11-03 03:41:01 +0000

    Update error handling mechanisms in the application.

--- utils.go
+++ utils.go
@@ -12,3 +12,21 @@ import (
  // fmt.Scanln(&category)	return strings.ToLower(category)
 }
 
+func getPlayerGuess() rune {
+	var guess string
+	for {
+		fmt.Print("Guess a letter: ")
+		fmt.Scanln(&guess)
+
+		if len(guess) != 1 || !isLetter(guess) {
+			fmt.Println("Please enter a valid single letter.")
+			continue
+		}
+		return rune(strings.ToLower(guess)[0])
+	}
+}
+
+func isLetter(s string) bool {
+	return len(s) == 1 && s[0] >= 'a' && s[0] <= 'z'
+}
+
